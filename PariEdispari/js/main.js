40fe58a998e7443b5e4a767a14a9a2c32221d37b
Hirisci <www.alan92@icloud.com>
2022-05-06 15:49:31 +0200

    Esercizio 2

--- PariEdispari/js/main.js
+++ PariEdispari/js/main.js
@@ -1,5 +1,63 @@
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
+/*╔══════════════════════════════════════════════════════════════════════════════════════════╗
+                                        FUNZIONI
+  ╚══════════════════════════════════════════════════════════════════════════════════════════╝*/
+  // ritorna un numero Random che parte da MIN e arriva sino a MAX 
+  function rndRangeNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+  // ritorna true se il numero é pari
+  function isEven(num){
+      if(num%2===0){
+          return true;
+      }
+    return false;
+  }
+
+
+/*╔══════════════════════════════════════════════════════════════════════════════════════════╗
+                                            MAIN
+  ╚══════════════════════════════════════════════════════════════════════════════════════════╝*/
+/* --------------------------------------------------------------------------------------------
+CONSEGNA EXERCIZIO
+
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+
+---------------------------------------------------------------------------------------------*/
+let winUser;
+do{
+    winUser = prompt("Scegli Pari o Dispari")
+    winUser = winUser.toLocaleLowerCase()
+} while( winUser !== "pari" &&  winUser !== "dispari")
+let numUser;
+do{
+    numUser = Number(prompt("inserisci un numero tra 1 e 5"));
+} while((numUser<1 || numUser>5) || isNaN(numUser)) 
+
+const numCPU = rndRangeNumber(1,6);
+const sommaNum = numCPU + numUser;
+console.log(winUser,numUser, numCPU, sommaNum)
+
+if(isEven(sommaNum)){
+      if(winUser==="pari"){
+          console.log("hai vinto!")
+      }else{
+        console.log('hai perso')
+      }
+    } else {
+        if(winUser==="dispari"){
+            console.log("hai vinto!")
+        }else{
+          console.log('hai perso')
+        }
+    }
+
+
+
+
+
+
