c334488cb74591836988ff327e9a420febf36261
Hirisci <www.alan92@icloud.com>
2022-05-06 14:31:53 +0200

    first commit

---
+++ Palindromo/js/main.js
@@ -0,0 +1,53 @@
+/*╔═══════════════╗
+      FUNZIONI
+  ╚═══════════════╝*/
+
+  function validateNumber(msg){
+    do{
+        let input = Number(prompt(msg))
+    }while(isNaN(input))
+
+    return input;
+  }
+
+  function isPalindroma(msg){
+      let controllore = true
+      for(let i =0 ; i<msg.length; i++){
+        //   if(msg[i]!==msg[msg.length-i]){
+        //       controllore = false;
+        //   }
+        console.log(msg[i], msg[msg.length-i-1])
+      }
+      return controllore;
+  }
+
+
+
+
+
+
+
+
+
+
+
+/*╔═══════════════╗
+        MAIN
+  ╚═══════════════╝*/
+/* --------------------------------------------------------------------------------------------
+CONSEGNA EXERCIZIO
+
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+---------------------------------------------------------------------------------------------*/
+
+
+const msg = prompt("Digita una parola Palindroma")
+if(isPalindroma(msg)){
+    console.log("La parola inserita é una parola Palindroma")
+}else{
+    console.log("La parola inserita é una parola Palindroma")
+}
+
